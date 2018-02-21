81ad40048dad14eaec93de63aeaedbac8d37bc8e
VincentKempers <vincent.m.kempers@gmail.com>
2018-02-21 15:15:36 +0100

    added script, im sorry ...

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * when the buttons get clicked they change the svg filter style on the body
+ * 
+ */
+let buttons = document.getElementsByTagName('button');
+let body = document.getElementsByTagName('body');
+
+
+
+
+for (var i =0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function(event) {
+    if (event.target.value) {
+      document.body.style.filter = `url(#${event.target.value})`;
+    }
+  }, true)
+}
